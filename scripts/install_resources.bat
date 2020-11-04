a0289b9971b9e5e4e0e757f0919cc6e92fb927f2
Adam Graham <adam.zigurous@gmail.com>
2020-11-03 18:09:35 -0600

    Include new resource packs and add script to install them

---
+++ scripts/install_resources.bat
@@ -0,0 +1,4 @@
+robocopy "../resourcepacks/" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks" /E
+robocopy "../shaderpacks/" "%UserProfile%\AppData\Roaming\.minecraft\shaderpacks" /E
+pause
+exit
