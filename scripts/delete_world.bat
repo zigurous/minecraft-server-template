c7e071712122591c8a5c0979b6e53a5180349aae
Adam Graham <adam@zigurous.com>
2021-03-18 05:26:59 -0500

    Add delete_world.bat script

--- scripts/delete_world.bat
+++ scripts/delete_world.bat
@@ -1,14 +1,13 @@
 setlocal
 
 :PROMPT
-set /P AREYOUSURE=Regenerating the world will permanently delete the current server data. This is very destructive! Are you sure you want to continue? (Y/[N])?
+set /P AREYOUSURE=Deleting the world will permanently delete the current server data. This is very destructive! Are you sure you want to continue? (Y/[N])?
 if /I "%AREYOUSURE%" neq "Y" goto END
 
 del /s /q "%~dp0..\map\*.*"
 del /s /q "%~dp0..\server\logs\*.*"
 del /s /q "%~dp0..\server\world\*.*"
 del "%~dp0..\server\usercache.json"
-call run.bat
 
 :END
 
