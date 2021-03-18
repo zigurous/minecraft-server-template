712746c0d189f3f0df081a36d76d65b5388d1f91
Adam Graham <adam@zigurous.com>
2021-03-18 05:22:25 -0500

    Install mods during install_resources.bat script

--- scripts/install_resources.bat
+++ scripts/install_resources.bat
@@ -1,3 +1,4 @@
+robocopy "%~dp0..\mods" "%UserProfile%\AppData\Roaming\.minecraft\mods" /E
 robocopy "%~dp0..\resourcepacks" "%UserProfile%\AppData\Roaming\.minecraft\resourcepacks" /E
 robocopy "%~dp0..\shaderpacks" "%UserProfile%\AppData\Roaming\.minecraft\shaderpacks" /E
 pause
